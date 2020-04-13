8407b36f5ac462c4d050eacee6a47f27300328e8
mixxamm <maximjanssensj5@gmail.com>
2020-04-14 00:01:00 +0200

    Verwijder perongeluk gecommite code
    Was van een ander project 🤦‍♂️

--- facilitap/android/app/src/main/java/be/apti/facilitap/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package be.apti.facilitap;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- facilitap/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kHalfText = TextStyle(color: Color(0x88000000));
